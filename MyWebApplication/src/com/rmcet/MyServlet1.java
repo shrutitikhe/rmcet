67c52bf74696b0fcac1cbf228233628fa18bda85
Shruti D. Tikhe <tikhesd@rmc.com>
2018-03-13 16:53:43 +0530

    MyWebApplication

---
+++ MyWebApplication/src/com/rmcet/MyServlet1.java
@@ -0,0 +1,55 @@
+package com.rmcet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MyServlet
+ */
+@WebServlet("/MyServlet1")
+public class MyServlet1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MyServlet1() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		
+		if(username.equals("Ranjit") && password.equals("password"))
+		{
+			request.setAttribute("name", username);
+			
+			RequestDispatcher rd=request.getRequestDispatcher("Success");
+			rd.forward(request, response);
+		}
+		else
+		{
+			response.sendRedirect("Error.html");
+		}
+		
+	}
+
+}
